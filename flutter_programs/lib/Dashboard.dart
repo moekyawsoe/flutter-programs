4646d7d941d00f67424ce779e9b3f29f7c007aba
moekyawsoe <moekyawsoe109@gmail.com>
2023-06-07 19:12:08 +0630

    simple button

--- flutter_programs/lib/Dashboard.dart
+++ flutter_programs/lib/Dashboard.dart
@@ -9,27 +9,14 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 350.0,
-        height: 250.0,
-        padding: const EdgeInsets.all(30.0),
-        margin: const EdgeInsets.all(30.0),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.blueGrey,
-            // borderRadius: BorderRadius.circular(10.0),
-            border: Border.all(color: Colors.green, width: 6.0),
-            shape: BoxShape.circle,
-            image: const DecorationImage(image: AssetImage("images/16623.jpg")),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade900,
-                blurRadius: 7,
-                spreadRadius: 5,
-                offset: const Offset(4, 4),
-              )
-            ]),
+        body: Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(20.0),
+            fixedSize: const Size(300, 80)),
+        child: const Text("Let's Begin"),
       ),
-    );
+    ));
   }
 }
