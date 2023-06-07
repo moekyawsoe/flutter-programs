8498da5f46283492999500af686cc69e66f71d9a
moekyawsoe <moekyawsoe109@gmail.com>
2023-06-07 13:12:11 +0630

    done

--- flutter_programs/lib/main.dart
+++ flutter_programs/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
       title: 'My Flutter App',
-      home: Center(
-        child: Text(
-          'Hello Word',
-          textDirection: TextDirection.ltr,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'.toUpperCase()),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: const Center(
+          child: Text.rich(TextSpan(text: 'My', children: [
+            TextSpan(
+                text: 'Flutter',
+                style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+            TextSpan(
+                text: 'App',
+                style: TextStyle(fontSize: 30.0, color: Colors.blue))
+          ])),
         ),
       )));
 }
