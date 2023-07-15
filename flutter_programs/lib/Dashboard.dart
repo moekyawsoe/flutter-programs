979124f9cf649281a62cf0df2e68fc96dab63898
moekyawsoe <moekyawsoe109@gmail.com>
2023-07-15 10:44:37 +0630

    Update Dashboard.dart

--- flutter_programs/lib/Dashboard.dart
+++ flutter_programs/lib/Dashboard.dart
@@ -9,42 +9,41 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
-        ),
-        title: const Text("Home"),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          // IconButton(onPressed: () {}, icon: const Icon(Icons.menu_book)),
-        ],
-        elevation: 0,
+        title: const Text("Rows and Columns"),
         centerTitle: true,
-        backgroundColor: Colors.purple.withOpacity(0.7),
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
-        // flexibleSpace: const Image(
-        //   image: NetworkImage(
-        //       "https://media.istockphoto.com/id/468993368/photo/house.jpg?s=612x612&w=0&k=20&c=sazQHQhVq1vR7nsoVmtDCAyXp8sZFhY_xoXrvVp8KCs="),
-        //   fit: BoxFit.cover,
-        // ),
       ),
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              "AppBar Styles",
-              style: TextStyle(fontSize: 22),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.star),
+                Icon(Icons.star),
+                Icon(Icons.star),
+                Icon(Icons.star_border),
+                Icon(Icons.star_border),
+              ],
             ),
-            Text(
-              "Coding with MKS",
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),
-            )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: const [
+                Icon(
+                  Icons.phone,
+                  size: 35.0,
+                ),
+                Icon(
+                  Icons.alt_route,
+                  size: 35.0,
+                ),
+                Icon(
+                  Icons.ice_skating,
+                  size: 35.0,
+                )
+              ],
+            ),
           ],
         ),
       ),
